11f38e03671d5d3ac1bcfba36aaab93959164dae
kanarysk <kanarysk@mail.com>
2018-11-17 11:34:05 -0800

    'js2-3'

---
+++ js2-3.js
@@ -0,0 +1,13 @@
+/*Create a function which returns square of a given number.
+sample:
+input-10 -> output-100
+*/
+
+var s = 2;
+
+function myFunction (n, s) {
+  var answer = n * s;
+  console.log(answer)
+}
+
+myFunction(5, n);
