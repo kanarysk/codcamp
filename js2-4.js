16b8c7b7f5d23b3f5d3617ecd92964783d31fe3a
kanarysk <kanarysk@mail.com>
2018-11-17 11:56:22 -0800

    'js2-4'

---
+++ js2-4.js
@@ -0,0 +1,12 @@
+/*
+write a function which converts given celsius to fahrenheit
+formula: (C × 9/5) + 32 = F
+*/
+var a = 1.8;
+
+function myFunction (C, F) {
+  var answer = (C * a) + 32;
+  console.log(answer)
+}
+
+myFunction(15, F);
